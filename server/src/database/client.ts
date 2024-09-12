f5dee0a47bbf00ab772f2d33784e9734e3443e5d
Julien RICHARD <julien.y.richard@gmail.com>
2024-09-12 11:10:47 +0200

    Mise en place de la config de gestion pour Docker et ajout de la BDD postgres

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -6,24 +6,24 @@ import { Status } from "../status/Status.entities"
 
 dotenv.config();
 
-const { BACKEND_DBFILE } = process.env;
+// const { BACKEND_DBFILE } = process.env;
 
-export const dataSource = new DataSource({
-  type: "sqlite",
-  database: `${BACKEND_DBFILE}`,
-  entities: [Repo, Lang, Status],
-  synchronize: true,
-});
-
-// const { DB_PASSWORD, DB_DATABASE, DB_USER, DB_HOST } = process.env;
-
-// export const dataSource= new DataSource({
-//   type: 'sqlite',
-//   host: DB_HOST,
-//   port: 5432,
-//   username: DB_USER,
-//   password: DB_PASSWORD,
-//   database: DB_DATABASE,
-//   synchronize: true,
+// export const dataSource = new DataSource({
+//   type: "sqlite",
+//   database: `${BACKEND_DBFILE}`,
 //   entities: [Repo, Lang, Status],
+//   synchronize: true,
 // });
+
+const { POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_USER, POSTGRES_HOST } = process.env;
+
+export const dataSource= new DataSource({
+  type: 'postgres',
+  host: POSTGRES_HOST,
+  port: 5432,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
+  synchronize: true,
+  entities: [Repo, Lang, Status],
+});
