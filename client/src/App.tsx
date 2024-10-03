4d89156cf7a81fa106d3dfd3bb8324ede9ddf289
Julien RICHARD <julien.y.richard@gmail.com>
2024-10-03 16:48:36 +0200

    Test de coonfig avec console.log

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
   const filterRepo = (filter: string | null) => {
     refetch({ filter });
     console.log("TEST de husky sur commit");
+    console.log("Nex test");
   };
 
   if (data) {
